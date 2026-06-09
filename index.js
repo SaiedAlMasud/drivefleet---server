b3cb83ae7c3686fc1b9d3a1363c3b035e81036ba
Ankon190 <ankon190@gmail.com>
2026-06-09 21:58:57 +0600

    fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -320,6 +319,4 @@ app.delete('/api/bookings/:id', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
-});
+module.exports = app;
